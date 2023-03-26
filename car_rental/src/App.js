a04f7e89bb530768c3f0fab7b2183c3c529e2adb
Hussain <hussainhafidh1@gmail.com>
2023-03-26 15:16:04 +0530

    added files

--- car_rental/src/App.js
+++ car_rental/src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import CarBooking from './pages/CarBooking';
+import BookingDetails from './pages/BookingDetails';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Quote from './pages/Quote';
+
 
 function App() {
   return (
-    <CarBooking/>
+    <div className='App'>
+
+      <BrowserRouter>
+      <Routes>
+         
+           <Route path='/carbooking' element={<CarBooking/>}></Route>
+           <Route path='/quote' element={<Quote/>}></Route>
+            <Route path="/BookingDetails" element={<BookingDetails/>}></Route>
+         
+          </Routes>
+      </BrowserRouter>
+
+    </div>
+
+    
   );
 }
 
--- car_rental/src/pages/CarBooking.js
+++ car_rental/src/pages/CarBooking.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from 'react';
+import data from '../data/data';
+import '../styles/CarBooking.css'
+
+import { Link } from 'react-router-dom';
+
 
 const CarBooking = () => {
-  return (
-    <div>CarBookg</div>
-  )
-}
+    return (
+        <div>
+            {/* <Header /> */}
+            {/* <hr /> */}
+            <span> Origin namee &gt; Destination namee &gt; 12 june-2019  -  13-june-2019
+                <Link to="/Quote"><button className='modify-btn'>Modify</button></Link>
+            </span>
+            {/* <hr /> */}
 
+            <section className='thirdNavbar'>
+
+                <select className='dropdown'>
+                    <option>
+                        Car Type
+                    </option>
+                    <option>
+                        XUV
+                    </option>
+                    <option>
+                        UV
+                    </option>
+                    <option>
+                        All
+                    </option>
+                </select>
+
+
+                <button className='btns'>Setting</button>
+                <button className='btns'>Milage</button>
+                <button className='btns'>Other</button>
+
+
+            </section>
+            <hr />
+
+
+
+            <section style={{ width: "100%" }}>
+
+                {
+                    data.map((item, index) => {
+                       
+                        return (
+                            <div key={index} className='cards'>
+
+                                <div className='image_box'>
+                                    <img src={item.img} alt={item.name} />
+                                </div>
+                                <p className='seat'>{item.seater} Persons</p>
+
+                                <div className='details'>
+
+                                    <h3>{item.name}</h3>
+                                    <p className='rate'>{item.rateperkm}Rs/km</p>
+
+                                </div>
+                                <div className='fare-btn'>
+                                    <p className='fare'>{item['fare']}</p>
+
+
+                                    <Link to='/BookingDetails'><button className='btn'>{item.Book}</button></Link>
+                                </div>
+
+                            </div>
+                        )
+                    })
+                }
+            </section>
+        </div >
+    )
+}
 export default CarBooking
