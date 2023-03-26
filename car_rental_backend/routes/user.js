abd125a08ccc4b61d508db43f85754713c79345b
parul-jaiswal2002 <paruljansie@gmail.com>
2023-03-26 15:52:06 +0530

    added backend functionality for login and signup

--- car_rental_backend/routes/user.js
+++ car_rental_backend/routes/user.js
@@ -1,1 +1,12 @@
-module.export
+const express = require('express')
+const { loginUser, signUpUser } = require('../controllers/userControllers');
+
+const router = express.Router();
+
+//login
+router.post('/login', loginUser)
+
+//signup
+router.post('/signup', signUpUser)
+
+module.exports = router
