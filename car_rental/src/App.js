0670ecea886839523f548938309eb4f88b4868f0
BharathPenkey <penkybharath@gmail.com>
2023-03-28 05:19:45 +0530

    added frontend functionality for addcars and added cssfiles

--- car_rental/src/App.js
+++ car_rental/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Quote from './pages/Quote';
 import Admin from './pages/Admin';
 import AddCars from './pages/AddCar';
+import EditCar from './pages/EditCar';
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
            <Route path='/carbooking' element={<CarBooking/>}/>
            <Route path='/quote' element={<Quote/>}/>
             <Route path="/BookingDetails" element={<BookingDetails/>}/>
+            <Route path="/editcardetails" element={<EditCar/>}/>
          
           </Routes>
       </BrowserRouter>
