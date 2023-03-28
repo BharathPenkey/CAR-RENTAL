722760f65c1372bb471102382b685872f9663ded
parul-jaiswal2002 <paruljansie@gmail.com>
2023-03-28 17:04:55 +0530

    added some changes in Bharats code

--- car_rental_backend/controllers/CarContollers.js
+++ car_rental_backend/controllers/CarContollers.js
@@ -7,21 +7,8 @@ const getALLCarDetails = async (req, res) => {
     const cars = await Car.find({}).sort({createdAt : -1})
     res.status(200).json(cars)
 }
-// const postdetails = async (req, res) => {
-//     const cars = await Car.find({}).sort({createdAt : -1})
-//     res.status(200).json(cars)
-// }
-const createNewBooking = async (req, res) => {
-    const {img,seater,name, innova,rateperkm,available_Date,Book,fare} = req.body
-    try{
-    
-       const newCar = await Car.create({img,seater,name, innova,rateperkm,available_Date,Book,fare}) 
-       res.status(200).json(newCar) 
-    }
-    catch(error){
-       res.status(400).json({error : error.message})
-    }
-}
+
+
 const createNewCarDetail = async (req, res) => {
     const {img,seater,name,rateperkm,available_Date,Book,fare} = req.body
     try{
@@ -64,6 +51,6 @@ const CreateAddCar =async (req,res)=>{
 
 
 module.exports = {
-    getALLCarDetails,createNewBooking,CreateAddCar,createNewCarDetail 
+    getALLCarDetails,CreateAddCar,createNewCarDetail 
 }
 
